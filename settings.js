b2c072164add431f7377a8861458b6dfeb2ed1f5
Urwa000786 <sa.as1100000786@gmail.com>
2024-09-11 20:59:40 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
-global.ytname = "YT: Tech-God"
-global.socialm = "GitHub: techgod143"
+global.ownernomer = "923313122070"
+global.ownername = "🐛Sardar "
+global.ytname = "YT: Sardar-Tech"
+global.socialm = "GitHub: Sardar786"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '923313122070'  //creator number
+global.ownername = '🐛Sardar Tach' //owner name
+global.botname = 'Sardar Tach ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Sardar-Tech\n\nContact: +923313122070'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -39,9 +39,9 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
